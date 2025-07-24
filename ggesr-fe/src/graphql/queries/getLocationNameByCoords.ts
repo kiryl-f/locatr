7a1cacea5db4220a16fb70a40373163cda367a2d
Kiryl Filipovich <fkiryl@gmail.com>
2025-07-24 17:42:09 +0300

    actual location name fetching query

--- ggesr-fe/src/graphql/queries/getLocationNameByCoords.ts
+++ ggesr-fe/src/graphql/queries/getLocationNameByCoords.ts
@@ -1,4 +1,4 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql } from '@apollo/client';
 
 export const LOCATION_NAME_BY_COORDS = gql`
   query locationNameByCoords($lat: Float!, $lon: Float!) {
