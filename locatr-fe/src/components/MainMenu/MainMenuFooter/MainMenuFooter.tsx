23617ed1e30c752ef3c9d8b95a83bf46482b0744
Kiryl Filipovich <fkiryl@gmail.com>
2025-07-27 21:25:51 +0300

    renaming project

--- locatr-fe/src/components/MainMenu/MainMenuFooter/MainMenuFooter.tsx
+++ locatr-fe/src/components/MainMenu/MainMenuFooter/MainMenuFooter.tsx
@@ -3,7 +3,7 @@ import styles from "./MainMenuFooter.module.scss";
 export default function MainMenuFooter() {
   return (
     <footer className={styles.footer}>
-      Built with ❤️ | <a href="https://github.com/kiryl-f/ggesr">GitHub</a>
+      Built with ❤️ | <a href="https://github.com/kiryl-f/locatr">GitHub</a>
     </footer>
   );
 } 
